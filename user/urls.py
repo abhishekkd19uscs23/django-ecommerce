0e60925310a086a4e3d8cfe5af5aab76fff7deb9
abhishekkd19uscs23 <abhishekmankave12@gmail.com>
2022-02-17 15:34:14 +0530

    main.html1 changed

---
+++ user/urls.py
@@ -0,0 +1,11 @@
+
+from django.urls import path
+from django.urls.conf import include
+from . import views
+
+urlpatterns = [
+    path('home',views.index),
+    path('nowayhome',views.index1)
+]
+    
+    
