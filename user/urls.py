05f15cc3a719646c29afde7ec72f06d87ffc9210
abhishekkd19uscs23 <abhishekmankave12@gmail.com>
2022-02-28 16:14:14 +0530

    nu

--- user/urls.py
+++ user/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     path('',views.index),
-    path('nowayhome',views.index1),
-    path('mutiverseofmadness',views.index3)
+    path('nowayhome',views.index1)
+
 
    
 ]
